4e11c68a3928c2d355fdad781e41dcdf0a1936b0
sospeterkedogo <kedogosospeter36@gmail.com>
2025-11-28 19:49:47 +0000

    fix: Correct About link href in Navbar component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
 
   const navLinks = [
     { name: "Work", href: "/#work" },
-    { name: "About", href: "/#about" },
+    { name: "About", href: "/about" },
     { name: "Insights", href: "/blog" },
   ];
 
